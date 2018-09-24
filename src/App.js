2f2fc79ca573f11d1fb0a1d6e0468940596bbb63
Jason Priddy <jason_priddy@yahoo.com>
2018-09-24 10:20:16 -0400

    committing all files to git for push to GitHub

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Header from './components/Header';
+import Contacts from './components/Contacts';
+import { Provider } from './context';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+        <div className="App">
+          <Header branding="Contact Manager" />
+
+          <Contacts />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
---
+++ src/context.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+// Context lets us pass a value deep into the component tree
+// without explicitly threading it through every component.
+// In other words, no need to hot potato state up or down the
+// component chain :)
+const Context = React.createContext();
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'DELETE_CONTACT':
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+        )
+      };
+    default:
+      return state;
+  }
+};
+
+// Provider to provide state management throughout the app life-cycle
+// Available to any components within the app once imported
+// Context provides a way to pass data through the component tree without having to pass props down manually at every level.
+export class Provider extends Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: 'John Doe',
+        email: 'jdoe@gmail.com',
+        phone: '555-555-5555'
+      },
+      {
+        id: 2,
+        name: 'Karen Williams',
+        email: 'kwilliams@gmail.com',
+        phone: '222-222-2222'
+      },
+      {
+        id: 3,
+        name: 'Henry Johnson',
+        email: 'henry@gmail.com',
+        phone: '111-111-1111'
+      }
+    ],
+    dispatch: action => {
+      this.setState(state => reducer(state, action));
+    }
+  };
+
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+// The context contains a consumer
+export const Consumer = Context.Consumer;
