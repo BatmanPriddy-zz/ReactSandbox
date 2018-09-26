597448ee7f66f4227fa184bb282f680665d32e23
Priddy <jason.priddy@cognex.com>
2018-09-26 19:31:55 -0400

    committing moving of files and settings changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
-import Contacts from './components/Contacts';
+import Header from './components/layout/Header';
+import Contacts from './components/contacts/Contacts';
 import { Provider } from './context';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
